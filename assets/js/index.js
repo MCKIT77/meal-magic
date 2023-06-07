ac9d9da201a7bcde054b1c465e359c72c0d7210f
Jacob McKitrick <jakemckit@gmail.com>
2023-06-07 19:58:56 -0400

    added starter api call

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+var containerEl = document.getElementById('container');
+
+
+var reqUrl = "https://api.edamam.com/api/recipes/v2?type=public&q='chicken''noodles'&app_id=3363fcd5&app_key=fee91a78bd642adc08094cc80cd02704&health=tree-nut-free&imageSize=SMALL"
+
+// function getReq() {
+//     fetch(reqUrl)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//     console.log(response);
+
+// }
