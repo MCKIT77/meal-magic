9d2a256875ad54a24b20a4bcce796033543391a2
Abdikalik Dini <adini18@gmail.com>
2023-06-07 22:51:24 -0400

    updated html with form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,14 @@
 var containerEl = document.getElementById('container');
 
 
-var reqUrl = "https://api.edamam.com/api/recipes/v2?type=public&q='chicken''noodles'&app_id=3363fcd5&app_key=fee91a78bd642adc08094cc80cd02704&health=tree-nut-free&imageSize=SMALL"
+var reqUrl = "https://api.edamam.com/api/recipes/v2?type=public&q='chicken''noodles'&app_id=3363fcd5&app_key=fee91a78bd642adc08094cc80cd02704&health=tree-nut-free&imageSize=SMALL";
 
-// function getReq() {
-//     fetch(reqUrl)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//     console.log(response);
+function getReq() {
+    fetch(reqUrl)
+        .then(function (response) {
+           console.log(response.json);
+        })
 
-// }
+}
+
+getReq();
