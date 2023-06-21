cc37718dd46fb97b86b2979a12bea0440c3f6497
Jacob McKitrick <jakemckit@gmail.com>
2023-06-21 18:15:30 -0400

    finishing touches

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # meal-magic
-This application will take input from a user and return a list of recipes that can be made with those ingredients
+This application will take input from a user and return a list of recipes that can be made what includes those ingredients
+
+## how-to-use
+You can type in multiple parameters just separate multiple words with a comma, once you have typed your chosen ingredients, you can choose dietary restrictions under the "diet" drop-down or allergy restrictions under the "allergies" drop down. Once satisfied, click "search". You will be shown results with ingredients you chose. If you see something you would like to make, click on the name and it will bring you to another page to show the ingredients, a youtube video (if one is available for that item), and the original link to the full recipe site. Once done you can click the "back to hompage" button to start over.
+
+## where-to-find
+important links:
+
+github repo: https://github.com/MCKIT77/meal-magic
+
+github pages: https://mckit77.github.io/meal-magic/
+
+## api-usage
+edamam api: "https://api.edamam.com/api/recipes/v2"
+
+youtube api: "https://www.googleapis.com/youtube/v3"
+
+## how-it-looks
+![HomePage](assets/img/finished-1.PNG)
+![Searched](assets/img/finished-2.PNG)
+![Redirection](assets/img/finished-3.PNG)
+![Redirection-2](assets/img/finished-4.PNG)
+
+
+
+
+
--- assets/js/endpoint.js
+++ assets/js/endpoint.js
@@ -20,12 +20,12 @@ $(document).ready(function () {
         titleEl.textContent = selectedCard.label;
         recipeContainer.appendChild(titleEl);
         // document.location.href = `?${selectedCard.label}`;
-        
+
 
 
 
         var imageEl = document.createElement('img');
-        imageEl.classList.add('card-img-main');
+        imageEl.classList.add('card-img-alt');
         imageEl.src = selectedCard.image;
         recipeContainer.appendChild(imageEl);
 
@@ -55,42 +55,42 @@ $(document).ready(function () {
         backBtn.textContent = 'Back to Homepage';
         recipeContainer.appendChild(backBtn);
 
-        var recipeQuery = document.querySelector('.card-header').textContent; 
+        var recipeQuery = document.querySelector('.card-header').textContent;
         // Create the API request URL using the video ID
         var youtubeApiKey = 'AIzaSyBL4yI-cDYDAwEF2igoJsxRKm_nJsBbyio';
         var youtubeApiUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + recipeQuery + '&key=' + youtubeApiKey;
         var videoContainerEl = document.getElementById('videoContainer');
-      
+
         // Sends the API request
         fetch(youtubeApiUrl)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-            // Extract the video ID from the API response
-            console.log(data)
-            var videoId = data.items[0].id.videoId;
-      
-            // Create the YouTube video URL
-            var youtubeUrl = 'https://www.youtube.com/watch?v=' + videoId;
-      
-            // Create the YouTube video iframe
-            var iframeEl = document.createElement('iframe');
-            iframeEl.width = '640';
-            iframeEl.height = '360';
-            iframeEl.src = 'https://www.youtube.com/embed/' + videoId;
-            iframeEl.frameborder = '0';
-            iframeEl.allowfullscreen = true;
-      
-            // Clear the video container
-            videoContainerEl.innerHTML = '';
-      
-            // Append the iframe to the video container
-            videoContainerEl.appendChild(iframeEl);
-          })
-          .catch(function (error) {
-            console.log('Error:', error);
-          });
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                // Extract the video ID from the API response
+                console.log(data)
+                var videoId = data.items[0].id.videoId;
+
+                // Create the YouTube video URL
+                var youtubeUrl = 'https://www.youtube.com/watch?v=' + videoId;
+
+                // Create the YouTube video iframe
+                var iframeEl = document.createElement('iframe');
+                iframeEl.width = '640';
+                iframeEl.height = '360';
+                iframeEl.src = 'https://www.youtube.com/embed/' + videoId;
+                iframeEl.frameborder = '0';
+                iframeEl.allowfullscreen = true;
+
+                // Clear the video container
+                videoContainerEl.innerHTML = '';
+
+                // Append the iframe to the video container
+                videoContainerEl.appendChild(iframeEl);
+            })
+            .catch(function (error) {
+                console.log('Error:', error);
+            });
 
     }
 
